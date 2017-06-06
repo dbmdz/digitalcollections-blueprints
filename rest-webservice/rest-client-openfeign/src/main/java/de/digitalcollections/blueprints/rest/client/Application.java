c4874f8d3b3529ce883b56c7c4eb18260f21be3f
Marcus Bitzl <marcus.bitzl@bsb-muenchen.de>
2017-06-06 17:24:29 +0200

    Add integration tests using docker container

--- rest-webservice/rest-client-openfeign/src/main/java/de/digitalcollections/blueprints/rest/client/Application.java
+++ rest-webservice/rest-client-openfeign/src/main/java/de/digitalcollections/blueprints/rest/client/Application.java
@@ -1,24 +1,17 @@
-package de.digitalcollections.blueprints.rest.client.frontend.impl.openfeign;
+package de.digitalcollections.blueprints.rest.client;
 
 import de.digitalcollections.blueprints.rest.common.model.impl.Greeting;
 import feign.Feign;
-import feign.Param;
-import feign.RequestLine;
 import feign.gson.GsonDecoder;
 
 public class Application {
 
-  interface HelloEndpoint {
-    @RequestLine("GET /hello?name={name}")
-    Greeting greeting(@Param("name") String name);
-  }
-
   public static void main(String... args) {
-    HelloEndpoint endpoint = Feign.builder()
+    HelloClient client = Feign.builder()
             .decoder(new GsonDecoder())
-            .target(HelloEndpoint.class, "http://localhost:9000");
+            .target(HelloClient.class, "http://localhost:9000");
 
-    final Greeting greeting = endpoint.greeting("Sepp");
+    final Greeting greeting = client.greeting("Sepp");
     System.out.println(greeting.getContent());
   }
 }
