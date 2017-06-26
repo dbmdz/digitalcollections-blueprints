5ac2ba125468d9745cc6ba9c5a8d3783d584db1c
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2017-06-26 14:00:58 +0200

    Fix configuration for the backend testcases

--- blueprint-springmvc-thymeleaf-javaconfig-security-database/crud-hibernate-business-impl/src/test/java/de/digitalcollections/blueprints/crud/config/SpringConfigBackendForTest.java
+++ blueprint-springmvc-thymeleaf-javaconfig-security-database/crud-hibernate-business-impl/src/test/java/de/digitalcollections/blueprints/crud/config/SpringConfigBackendForTest.java
@@ -2,6 +2,7 @@ package de.digitalcollections.blueprints.crud.config;
 
 import de.digitalcollections.blueprints.crud.backend.api.repository.OperationRepository;
 import de.digitalcollections.blueprints.crud.backend.api.repository.RoleRepository;
+import de.digitalcollections.blueprints.crud.backend.api.repository.TextContentRepository;
 import de.digitalcollections.blueprints.crud.backend.api.repository.UserRepository;
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
@@ -21,6 +22,11 @@ public class SpringConfigBackendForTest {
   @Bean
   public RoleRepository roleRepository() {
     return Mockito.mock(RoleRepository.class);
+  }
+
+  @Bean
+  public TextContentRepository textContentRepository() {
+    return Mockito.mock(TextContentRepository.class);
   }
 
   @Bean
