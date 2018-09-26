08ed79e339f7e41fb2bbc6276e4c0ac2f08192c2
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2018-09-26 15:56:25 +0200

    Make endpoints health and info unauthorized

--- blueprint4-springboot-webapp/step04/src/main/java/de/digitalcollections/blueprints/webapp/springboot/config/SpringConfigSecurity.java
+++ blueprint4-springboot-webapp/step04/src/main/java/de/digitalcollections/blueprints/webapp/springboot/config/SpringConfigSecurity.java
@@ -1,6 +1,8 @@
 package de.digitalcollections.blueprints.webapp.springboot.config;
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.actuate.info.InfoEndpoint;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,8 +12,9 @@ public class SpringConfigSecurity extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.requestMatcher(EndpointRequest.toAnyEndpoint()).authorizeRequests()
-            .anyRequest().hasRole("ENDPOINT_ADMIN")
+    http.authorizeRequests()
+            .requestMatchers(EndpointRequest.to(InfoEndpoint.class, HealthEndpoint.class)).permitAll()
+            .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ENDPOINT_ADMIN")
             .and()
             .httpBasic();
   }
