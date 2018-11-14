12c7222b45dec5317da2195629fa9e75d6ff28c3
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2018-11-14 10:51:30 +0100

    Add jolokia security rule and allow access to all monitoring endpoints

--- webapp-springboot-thymeleaf/step10/src/main/java/de/digitalcollections/blueprints/webapp/springboot/config/SpringConfigSecurity.java
+++ webapp-springboot-thymeleaf/step10/src/main/java/de/digitalcollections/blueprints/webapp/springboot/config/SpringConfigSecurity.java
@@ -2,12 +2,11 @@ package de.digitalcollections.blueprints.webapp.springboot.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
-import org.springframework.boot.actuate.health.HealthEndpoint;
-import org.springframework.boot.actuate.info.InfoEndpoint;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SpringConfigSecurity extends WebSecurityConfigurerAdapter {
@@ -17,11 +16,15 @@ public class SpringConfigSecurity extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-            .requestMatchers(EndpointRequest.to(InfoEndpoint.class, HealthEndpoint.class)).permitAll()
-            .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ENDPOINT_ADMIN")
-            .and()
-            .httpBasic();
+    http
+            .authorizeRequests()
+            .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+            .requestMatchers(EndpointRequest.to("jolokia")).permitAll()
+            .antMatchers("/").permitAll()
+            .anyRequest().authenticated()
+            .and().httpBasic()
+            .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and().csrf().disable();
   }
 
   @Override
