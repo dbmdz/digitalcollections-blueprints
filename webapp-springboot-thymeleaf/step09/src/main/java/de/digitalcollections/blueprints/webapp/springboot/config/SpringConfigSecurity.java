32b2e50760c160d017b57e646190917ee09f4fbf
Ralf Eichinger <ralf.eichinger@bsb-muenchen.de>
2018-11-14 13:20:19 +0100

    Simplify spring security configuration (no csrf configuration needed in this blueprint)

--- webapp-springboot-thymeleaf/step09/src/main/java/de/digitalcollections/blueprints/webapp/springboot/config/SpringConfigSecurity.java
+++ webapp-springboot-thymeleaf/step09/src/main/java/de/digitalcollections/blueprints/webapp/springboot/config/SpringConfigSecurity.java
@@ -2,11 +2,12 @@ package de.digitalcollections.blueprints.webapp.springboot.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.health.HealthEndpoint;
+import org.springframework.boot.actuate.info.InfoEndpoint;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SpringConfigSecurity extends WebSecurityConfigurerAdapter {
@@ -16,15 +17,12 @@ public class SpringConfigSecurity extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http
-            .authorizeRequests()
-            .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+    http.authorizeRequests()
+            .requestMatchers(EndpointRequest.to(InfoEndpoint.class, HealthEndpoint.class)).permitAll()
             .requestMatchers(EndpointRequest.to("jolokia")).permitAll()
-            .antMatchers("/").permitAll()
-            .anyRequest().authenticated()
-            .and().httpBasic()
-            .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            .and().csrf().disable();
+            .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ENDPOINT_ADMIN")
+            .and()
+            .httpBasic();
   }
 
   @Override
