65a3d0cbcb18f82d13ddf329ec4a85d954148e1a
Stefan Schweter <stefan.schweter@bsb-muenchen.de>
2018-12-07 14:42:26 +0100

    springmvc: migrate unit tests to JUnit5 and include mockito and hamcrest

--- springmvc-thymeleaf/src/test/java/de/digitalcollections/blueprints/springmvc/frontend/webgui/AppTests.java
+++ springmvc-thymeleaf/src/test/java/de/digitalcollections/blueprints/springmvc/frontend/webgui/AppTests.java
@@ -1,31 +1,31 @@
 package de.digitalcollections.blueprints.springmvc.frontend.webgui;
 
 import de.digitalcollections.blueprints.springmvc.config.SpringConfigWeb;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
-import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebAppConfiguration
 @ContextConfiguration(classes = {SpringConfigWeb.class})
 public class AppTests {
 
-  private MockMvc mockMvc;
-
   @Autowired
   protected WebApplicationContext wac;
+  private MockMvc mockMvc;
 
-  @Before
+  @BeforeEach
   public void setup() {
     this.mockMvc = webAppContextSetup(this.wac).build();
   }
