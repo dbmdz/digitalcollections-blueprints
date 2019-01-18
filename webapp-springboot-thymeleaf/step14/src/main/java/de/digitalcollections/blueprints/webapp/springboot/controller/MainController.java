e25c4159b0df2bf3b77238ced0cb0c8feabfc65c
Stefan Schweter <stefan.schweter@bsb-muenchen.de>
2019-01-18 16:21:08 +0100

    controller: add small example of null pointer dereference 😲

--- webapp-springboot-thymeleaf/step14/src/main/java/de/digitalcollections/blueprints/webapp/springboot/controller/MainController.java
+++ webapp-springboot-thymeleaf/step14/src/main/java/de/digitalcollections/blueprints/webapp/springboot/controller/MainController.java
@@ -12,10 +12,22 @@ public class MainController {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MainController.class);
 
+  private static int getLength(int i) {
+    String s = null;
+
+    if (i == 1) {
+      s = "1";
+    } else if (i == 2) {
+      s = "2";
+    }
+    return s.length();
+  }
+
   @GetMapping(value = {"", "/"})
   public String printWelcome(Model model) {
     LOGGER.info("Homepage requested");
     model.addAttribute("time", new Date());
+    model.addAttribute("s", getLength(5));
     return "main";
   }
 }
